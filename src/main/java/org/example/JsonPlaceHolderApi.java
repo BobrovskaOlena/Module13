098655be289989484b84661eaf5fc27557726689
BobrovskaOlena <lena69114@gmail.com>
2023-03-25 00:23:51 +0200

    3

--- src/main/java/org/example/JsonPlaceHolderApi.java
+++ src/main/java/org/example/JsonPlaceHolderApi.java
@@ -1,4 +1,6 @@
 package org.example;
+import model.User;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,7 +9,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class JsonPlaceHolderApi {
-
+private User user;
     public static void main(String[] args) throws IOException {
 
         URL url = new URL("https://jsonplaceholder.typicode.com/users");
@@ -16,7 +18,24 @@ public class JsonPlaceHolderApi {
         connection.setRequestProperty("Content-Type", "application/json");
 
         // Тіло запиту - JSON з даними для нового об'єкту
-        String body = "{\"name\": \"Test User\", \"username\": \"testuser\", \"email\": \"testuser@gmail.com\"}";
+        String body = "User {\"id\": \"11\", " +
+                "\"name\": \"Test User\"," +
+                " \"username\": \"TestUser\", " +
+                "\"email\": \"testuser@gmail.com\", " +
+                "\"address\":{\n" +
+                "      \"street\": \"Kul Light\",\n" +
+                "      \"suite\": \"Apt. 6\",\n" +
+                "      \"city\": \"Lviv" +
+                "      \"zipcode\": \"92998-3874\",\n" +
+                "      \"geo\": {\n" +
+                "        \"lat\": \"-37.3159\",\n" +
+                "        \"lng\": \"81.1496\"}";
+
+                "\"address\": "+address+"\", " +
+                "\"phone\": "+phone+"\", " +
+                "\"website\": "+website+"\", " +
+                "\"company\": "+company+ " }";
+              //
 
         connection.setDoOutput(true);
         OutputStream os = connection.getOutputStream();
